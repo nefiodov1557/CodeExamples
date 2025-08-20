dce88d2b783868d3833db6db058475df04e43973
nefiodov1557 <102519917+nefiodov1557@users.noreply.github.com>
2025-08-20 22:07:23 +0300

    Add files via upload

---
+++ src/Models/User.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Tools.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+} 
